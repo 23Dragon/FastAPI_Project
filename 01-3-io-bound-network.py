c1db987a3482ec2f7714bc79b27dc6482397a5ff
23Dragon <wotnwogus@naver.com>
2022-10-21 13:09:41 +0900

    test

--- 01-3-io-bound-network.py
+++ 01-3-io-bound-network.py
@@ -1,11 +1,20 @@
 import requests
 
+proxies = {
+    "http": "",
+    "https": "",
+}
+
 
 def io_bound_func():
-    result = requests.get("http://google.com")
-    return result
+    response = requests.get("https://google.com", proxies=proxies)
+
+    # print(response.get(url))
+    return response
 
 
 if __name__ == "__main__":
+
     result = io_bound_func()
+
     print(result)
